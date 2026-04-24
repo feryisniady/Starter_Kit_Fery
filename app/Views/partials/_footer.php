35d1d2e300e8bb3dab1846e7e4c14f41e07e7d3b
inspektorat <fery@sampangkab.go.id>
2026-04-24 08:31:28 +0700

    update: Footer

--- app/Views/partials/_footer.php
+++ app/Views/partials/_footer.php
@@ -3,6 +3,6 @@
         &copy; <?= date('Y') ?> <b>RBAC Starter Kit</b> — CodeIgniter 4
     </div>
     <div class="footer-right">
-        Fery Isniady, S.Kom ❤️
+        Fery Isniady, S.Kom, MT ❤️
     </div>
 </footer>
